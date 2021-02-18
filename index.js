e28facbc48b972ddca684e9c380ef64f8da7c6ff
Amonrujee Buasungnoen <61160131@go.buu.ac.th>
2021-02-18 18:25:11 +0700

    installed required packages

---
+++ index.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient
+const express = require('express')
+
+const app = express()
+const url = 'mongodb+srv://superadmin:31122541ying@cluster0.g0htt.mongodb.net/sample_weatherdata?retryWrites=true&w=majority'
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+async function connect() {
+    await client.connect()
+}
+connect()
+
+app.get('/weather', async(req, res) => {
+    try {
+        const callLetters = req.query.callLetters
+        const qualityControlProcess = req.query.qualityControlProcess
+        const db = client.db('sample_weatherdata')
+        const collection = db.collection('data')
+        let query = {}
+
+        if (callLetters) {
+            query.callLetters = callLetters
+        }
+
+        if (qualityControlProcess) {
+            query.qualityControlProcess = qualityControlProcess
+        }
+
+        const cursor = collection.find(query).limit(10)
+        let data = []
+        await cursor.forEach(doc => data.push(doc.name))
+
+        res.send(data)
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+app.listen(3000, console.log('Start application at port 3000'))
